974bc09412f351f4e11fb7a83a439bd5c1d23dfc
Loise Mwangi <wanjirumwangi.learn@gmail.com>
2019-01-11 11:29:37 +0300

    display vote count on clicking like links

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,13 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
+
+
+
 export class QuoteComponent implements OnInit {
 
-  constructor() { }
+  @HostBinding('attr.class') cssClass ='row';
+  quote: string;
+  author: string;
+  contributor: string;
+  votes: number;
+
+  // default attributes
+  constructor() { 
+    this.quote = 'Optimism is the faith that leads to achievement. Nothing can be done without hope and confidence.';
+    this.author = 'Helen Keller';
+    this.contributor = 'Hope Munene';
+    this.votes = 0;
+  }
+
+
+  // when i click the like link add one vote to the votes counter
+  voteLike(){
+    this.votes = +1;
+    return false;
+
+  }
+
+  // when i click the dislike link remove one vote to the votes counter
+  voteDislike(){
+    this.votes = -1;
+    return false;
+    
+  }
 
   ngOnInit() {
   }
